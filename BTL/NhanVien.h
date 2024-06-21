2d00a975f38a3be8cd28280c023a001510cd19cd
Vinhneeeeee <154726414+vinhnhaa@users.noreply.github.com>
2024-06-21 17:05:37 +0700

    NhanVien.h

--- BTL/NhanVien.h
+++ BTL/NhanVien.h
@@ -2,12 +2,27 @@
 #define NHANVIEN_H
 
 #include "User.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
 
 class NhanVien : public User {
+private:
+    std::vector<std::string> CaTruc;
+
 public:
     NhanVien();
-    NhanVien(string id, string name);
+    NhanVien(std::string id, std::string name);
+
     void displayInfo() const override;
+    void loadFromFile(std::ifstream &file);
+    void hienThiCaTruc(const std::string& rootPath) const;
+    void hienThiLuong(const std::string& rootPath) const;
+
 };
 
+void giaoDienNhanVien(NhanVien* nv);
+
 #endif // NHANVIEN_H
+
