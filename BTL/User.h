63b0b46312575273d3048aaae9f5a8c5a9e005e7
Vinhneeeeee <154726414+vinhnhaa@users.noreply.github.com>
2024-06-30 15:24:22 +0700

    Update

--- BTL/User.h
+++ BTL/User.h
@@ -1,3 +1,4 @@
+
 #ifndef USER_H
 #define USER_H
 
@@ -9,13 +10,18 @@ class User {
 protected:
     string id;
     string name;
+    string ngaySinh;
+    string gioiTinh;
 
 public:
     User() {}
-    User(string id, string name) : id(id), name(name) {}
+    User(string id, string name, string ngaySinh, string gioiTinh)
+        : id(id), name(name), ngaySinh(ngaySinh), gioiTinh(gioiTinh) {}
     virtual void displayInfo() const = 0;
     string getId() const { return id; }
     string getName() const { return name; }
+    string getNgaySinh() const { return ngaySinh; }
+    string getGioiTinh() const { return gioiTinh; }
 };
 
 #endif // USER_H
